2ab2cf9e408c21ac9336d772614c49060ba0dbdf
Lucas Peer <Lucaspeer2008@hotmil.com>
2023-03-12 18:23:18 -0300

    sss

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
 
-const Cart = () =>{
+const Cart = () => {
     const{ cart, totalPrice} = useCartContext();
 
     if(cart.lenght === 0) {
         return(
             <>
             <p>No hay elementos en el carrito</p>
-            <Link to= '/'>Hacer compras</Link>
+            <Link to='/'>Hacer compras</Link>
             </>
         );
     }
--- src/components/ItemCart/index.jsx
+++ src/components/ItemCart/index.jsx
@@ -14,7 +14,7 @@ const itemCart = ({ product }) => {
                 <p>Titulo: {product.title}</p>
                 <p>Cantidad: {product.quantity}</p>
                 <p>Precio u.:{product.price} </p>
-                <p>Subtotal: </p>
+                <p>Subtotal: ${product.quantity * product.price} </p>
                 <button onClick={ () =>removeProduct(product.id)}>Eliminar</button>
             </div>
         </div>
