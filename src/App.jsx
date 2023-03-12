5ffe28a91aafc53432e434ef8676dda63f888306
Lucas Peer <Lucaspeer2008@hotmil.com>
2023-03-12 17:20:58 -0300

    b

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,22 @@ import Cart from './components/Cart';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
-
-
-
+import CartProvider from './context/CartContext';
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<ItemListContainer />} />
-          <Route path='/categoria/:artistasId' element={<ItemListContainer />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
-        </Routes>
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} />
+            <Route path='/categoria/:artistasId' element={<ItemListContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
 
     </>
